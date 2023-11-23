6ed28895c29632ecf4c9348927d8f6c6d73fd262
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2023-11-23 21:48:42 +0100

    debug: turn off email client

--- function_app.py
+++ function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 from check_for_internships import check_internship_availability, download_html
 import logging
 import os
-from azure.communication.email import EmailClient
+# from azure.communication.email import EmailClient
 
 app = func.FunctionApp()
 
@@ -17,7 +17,7 @@ def function(mytimer: func.TimerRequest) -> None:
     logging.info(check_internship_availability(html))
 
     connection_string = os.getenv('COMMUNICATION_CONNECTION_STRING')
-    email_client = EmailClient.from_connection_string(connection_string)
+    # email_client = EmailClient.from_connection_string(connection_string)
 
     logging.info(os.getenv('RECEIVER_EMAIL'))
 
