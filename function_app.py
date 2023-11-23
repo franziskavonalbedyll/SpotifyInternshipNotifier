088728e35bca1159e7fd9ef17825f1ce32b6b23d
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2023-11-23 21:46:33 +0100

    feat: add email client

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 from check_for_internships import check_internship_availability, download_html
 import logging
 import os
+from azure.communication.email import EmailClient
 
 app = func.FunctionApp()
 
@@ -14,6 +15,9 @@ def function(mytimer: func.TimerRequest) -> None:
     logging.info("Starting function ...")
     html = download_html(url)
     logging.info(check_internship_availability(html))
+
+    connection_string = os.getenv('COMMUNICATION_CONNECTION_STRING')
+    email_client = EmailClient.from_connection_string(connection_string)
 
     logging.info(os.getenv('RECEIVER_EMAIL'))
 
